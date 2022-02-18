b501ee9786c7bc3c24830253996ad274f3d7bcc2
carlosdan <carloschoqque@gmail.com>
2022-02-17 21:32:59 -0500

    f

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose')
 
 const {NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE} = process.env
-const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}` || `mongodb://localhost/notes-app`
+const MONGODB_URI = `mongodb://localhost/notes-app`
 
 mongoose.connect(MONGODB_URI , {
     useUnifiedTopology : true,
